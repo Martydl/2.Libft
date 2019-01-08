07343d00b6f133288d30f1e92c6861b372d31a7a
Martin DE LAGARDE <mde-laga@e3r1p11.42.fr>
2019-01-08 14:44:00 +0100

    Libft

---
+++ ft_strjoin.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mde-laga <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/16 12:58:01 by mde-laga          #+#    #+#             */
+/*   Updated: 2018/11/19 11:24:39 by mde-laga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*s3;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	i = ft_strlen((char*)s1) + ft_strlen((char*)s2) + 1;
+	if (!(s3 = (char*)malloc(sizeof(char) * i)))
+		return (NULL);
+	i = -1;
+	while (s1[++i])
+		s3[i] = s1[i];
+	j = -1;
+	while (s2[++j])
+		s3[i++] = s2[j];
+	s3[i] = '\0';
+	return (s3);
+}
